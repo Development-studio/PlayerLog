7c5c5ebb48c7c7ac6125f1b9612e7511ace46139
MisterCron <komron-at@mail.ru>
2021-09-05 20:29:55 +0700

    add

---
+++ PlayerLog.lua
@@ -0,0 +1,46 @@
+
+local filePlayerLog = './plugins/PlayerLog/PlayerLog.log'
+local fileIP = './plugins/PlayerLog/IP.log'
+local fileClientId = './plugins/PlayerLog/ClientId.log'
+
+data.openConfig(filePlayerLog)
+data.openConfig(fileIP)
+data.openConfig(fileClientId)
+    
+function PlayerLog(pl)
+    local A = pl:getDevice()
+    local D = A.os
+    file.writeLine(filePlayerLog, '[name: ' .. pl.realName .. ', uuid: ' .. pl.uuid .. ', xuid: ' .. pl.xuid .. ', data: ' .. system.getTimeStr() .. ', OS: ' .. D .. ']')
+end
+
+function IP(pl)
+    local A = pl:getDevice()
+    local B = A.ip
+    file.writeLine(fileIP, '[name: ' .. pl.realName .. 'ip: ' .. B .. ', data: ' .. system.getTimeStr() .. ']')
+   
+end
+
+
+function ClientId(pl)
+    local A = pl:getDevice()
+    local E = A.clientId
+    file.writeLine(fileClientId, '[name: ' .. pl.realName .. ', data: ' .. system.getTimeStr() .. ', clientId: ' .. E .. ']')
+    
+end
+
+mc.listen("onJoin", function(pl)
+    IP(pl)
+    ClientId(pl)
+    mc.runcmd("scoreboard objectives add old dummy")
+    if mc.runcmd('execute @a[name="'..pl.name..'",scores={old=1}] ~ ~ ~ title "'..pl.name..'" title §a') == true then
+        pl:tell('§c§aДобро пожаловать на сервер §l§c"'..pl.name..'"', 0)
+    else
+        mc.runcmd('execute @a[name="'..pl.name..'"] ~ ~ ~ scoreboard players add @s old 0')
+        pl:tell("§c§aСпасибо что зашли к нам впервые - §fDiscord: §ediscord.gg/uXvsVWW!", 0)
+        PlayerLog(pl)
+        mc.runcmd('execute @a[name="'..pl.name..'",scores={old=0}] ~ ~ ~ scoreboard players set "'..pl.name..'" old 1')
+
+    end
+end)
+
+print('[\27[92mCRON\27[0m] \27[93mPlayerLog loaded\27[0m')
---
+++ README.md
@@ -0,0 +1,7 @@
+# ping plugin
+
+
+## Installation
+1. Place PlayerLog.lua in the plugins directory.
+2. This plugin collects information about the player: `name, uuid, xuid, OS, ip, ClientID`.
+3. the information is saved in the `PlayerLog` folder
